8bc2274db5415d8d5b3154444643216516be3441
isglx69 <isglx69@yahoo.com>
2019-03-10 14:46:29 +0200

    hypermarket: #6/10.03.2019

--- hypermarket-core/src/main/java/ro/sda/hypermarket/core/dao/SupplierDAOImpl.java
+++ hypermarket-core/src/main/java/ro/sda/hypermarket/core/dao/SupplierDAOImpl.java
@@ -6,7 +6,6 @@ import org.hibernate.criterion.Restrictions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import ro.sda.hypermarket.core.entity.Supplier;
-
 import javax.transaction.Transactional;
 import java.util.List;
 
